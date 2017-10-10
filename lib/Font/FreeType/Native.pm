bde0742578e6ae36ebc3c4878ad6337b7caaf81b
David Warring <david.warring@gmail.com>
2017-10-11 10:59:18 +1300

    collect and populate enums

--- lib/Font/FreeType/Native.pm
+++ lib/Font/FreeType/Native.pm
@@ -27,6 +27,73 @@ constant FT_ULong  is export = ulong;
 constant FT_UShort = uint16;
 constant FT_F26Dot6 is export = long;
 constant FT_Glyph_Format = int32; # enum
+constant FT_Render_Mode = int32; # enum
+
+sub ft-code(Str $s) {
+    my uint32 $enc = 0;
+    for $s.ords {
+        $enc *= 256;
+        $enc += $_;
+    }
+    $enc;
+}
+
+enum FT_ENCODING is export «
+    :FT_ENCODING_NONE(0)
+    :FT_ENCODING_SYMBOL(ft-code("symb"))
+    :FT_ENCODING_UNICODE(ft-code("unic"))
+    :FT_ENCODING_SJIS(ft-code("sjis"))
+    :FT_ENCODING_PRC(ft-code("gb  "))
+    :FT_ENCODING_BIG5(ft-code("big5"))
+    :FT_ENCODING_WANGSUNG(ft-code("wang"))
+    :FT_ENCODING_JOHAB(ft-code("joha"))
+    :FT_ENCODING_ADOBE_STANDARD(ft-code("ADOB"))
+    :FT_ENCODING_ADOBE_EXPERT(ft-code("ADBE"))
+    :FT_ENCODING_ADOBE_CUSTOM(ft-code("ADBC"))
+    :FT_ENCODING_ADOBE_LATIN_1(ft-code("lat1"))
+    :FT_ENCODING_OLD_LATIN_2(ft-code("lat2"))
+    :FT_ENCODING_APPLE_ROMAN(ft-code("armn"))
+    »;
+
+enum FT_FACE_FLAG is export «
+    :FT_FACE_FLAG_SCALABLE(1 +<  0)
+    :FT_FACE_FLAG_FIXED_SIZES(1 +<  1)
+    :FT_FACE_FLAG_FIXED_WIDTH(1 +<  2)
+    :FT_FACE_FLAG_SFNT(1 +<  3)
+    :FT_FACE_FLAG_HORIZONTAL(1 +<  4)
+    :FT_FACE_FLAG_VERTICAL(1 +<  5)
+    :FT_FACE_FLAG_KERNING(1 +<  6)
+    :FT_FACE_FLAG_FAST_GLYPHS(1 +<  7)
+    :FT_FACE_FLAG_MULTIPLE_MASTERS(1 +<  8)
+    :FT_FACE_FLAG_GLYPH_NAMES(1 +<  9)
+    :FT_FACE_FLAG_EXTERNAL_STREAM(1 +< 10)
+    :FT_FACE_FLAG_HINTER(1 +< 11)
+    :FT_FACE_FLAG_CID_KEYED(1 +< 12)
+    :FT_FACE_FLAG_TRICKY(1 +< 13)
+    :FT_FACE_FLAG_COLOR(1 +< 14)
+    »;
+
+enum FT_GLYPH_FORMAT is export «
+    :FT_GLYPH_FORMAT_NONE(0)
+    :FT_GLYPH_FORMAT_COMPOSITE(ft-code('comp'))
+    :FT_GLYPH_FORMAT_BITMAP(ft-code('bits'))
+    :FT_GLYPH_FORMAT_OUTLINE(ft-code('outl'))
+    :FT_GLYPH_FORMAT_PLOT(ft-code('plot'))
+    »;
+
+enum FT_RENDER_MODE is export «
+    :FT_RENDER_MODE_NORMAL
+    :FT_RENDER_MODE_LIGHT
+    :FT_RENDER_MODE_MONO
+    :FT_RENDER_MODE_LCD
+    :FT_RENDER_MODE_LCD_V
+    :FT_RENDER_MODE_MAX
+    »;
+
+enum FT_STYLE_FLAG is export «
+    :FT_STYLE_FLAG_ITALIC( 1 +< 0 )
+    :FT_STYLE_FLAG_BOLD( 1 +< 1 )
+    »;
 
 class FT_Face is repr('CStruct') {...}
 class FT_Library is repr('CPointer') {...}
@@ -40,19 +107,6 @@ class FT_Bitmap_Size is repr('CStruct') is export {
     has FT_Pos    $.x-ppem;
     has FT_Pos    $.y-ppem;
 }
-
-sub ft-enc-tag(Str $s) {
-    my uint32 $enc = 0;
-    for $s.ords {
-        $enc *= 256;
-        $enc += $_;
-    }
-    $enc;
-}
-
-enum FT_ENCODING is export (
-    FT_ENCODING_UNICODE => ft-enc-tag("unic"),
-    );
 
 class FT_CharMap is export is repr('CStruct') {
     has FT_Face      $.face;
@@ -167,8 +221,10 @@ class FT_GlyphSlot is repr('CStruct') is export {
 
     has FT_Slot_Internal  $.internal;
 
-    submethod TWEAK {
-    }
+    method FT_Render_Glyph(
+        FT_Render_Mode $render-mode )
+    returns FT_Error is native($ftlib) {*};
+
 }
 
 class FT_SfntName is repr('CStruct') is export {
@@ -180,29 +236,6 @@ class FT_SfntName is repr('CStruct') is export {
     has CArray[FT_Byte]   $.string;      #| this string is *not* null-terminated! */
     has FT_UInt   $.string-len;  #| in bytes                              */
 }
-
-enum FT_FACE_FLAG is export «
-    :FT_FACE_FLAG_SCALABLE(1 +<  0)
-    :FT_FACE_FLAG_FIXED_SIZES(1 +<  1)
-    :FT_FACE_FLAG_FIXED_WIDTH(1 +<  2)
-    :FT_FACE_FLAG_SFNT(1 +<  3)
-    :FT_FACE_FLAG_HORIZONTAL(1 +<  4)
-    :FT_FACE_FLAG_VERTICAL(1 +<  5)
-    :FT_FACE_FLAG_KERNING(1 +<  6)
-    :FT_FACE_FLAG_FAST_GLYPHS(1 +<  7)
-    :FT_FACE_FLAG_MULTIPLE_MASTERS(1 +<  8)
-    :FT_FACE_FLAG_GLYPH_NAMES(1 +<  9)
-    :FT_FACE_FLAG_EXTERNAL_STREAM(1 +< 10)
-    :FT_FACE_FLAG_HINTER(1 +< 11)
-    :FT_FACE_FLAG_CID_KEYED(1 +< 12)
-    :FT_FACE_FLAG_TRICKY(1 +< 13)
-    :FT_FACE_FLAG_COLOR(1 +< 14)
-    »;
-
-enum FT_STYLE_FLAG is export «
-    :FT_STYLE_FLAG_ITALIC( 1 +< 0 )
-    :FT_STYLE_FLAG_BOLD( 1 +< 1 )
-    »;
 
 class FT_Face is export {
     has FT_Long           $.num-faces;
@@ -224,7 +257,7 @@ class FT_Face is export {
 
     HAS FT_Generic        $.generic;
 ##
-##    /*# The following member variables (down to `underline_thickness') */
+##    /*# The following member variables (down to `underline-thickness') */
 ##    /*# are only relevant to scalable outlines; cf. @FT_Bitmap_Size    */
 ##    /*# for bitmap fonts.                                              */
     HAS FT_BBox           $.bbox;
