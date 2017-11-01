8a389d762d2ddd91d6cf00233fbbd7c15fb93894
David Warring <david.warring@gmail.com>
2017-11-01 19:42:18 +1300

    fix case of FT_CharMap

--- lib/Font/FreeType/CharMap.pm
+++ lib/Font/FreeType/CharMap.pm
@@ -2,5 +2,5 @@ use NativeCall;
 use Font::FreeType::Native;
 
 class Font::FreeType::CharMap {
-    has FT_Charmap $.struct handles <platform-id encoding-id encoding>;
+    has FT_CharMap $.struct handles <platform-id encoding-id encoding>;
 }
